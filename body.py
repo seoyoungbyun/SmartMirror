730ba1b712f9b2b044ef26ca9b96054af7804dab
seoyoungbyun <qustjdud2181@gmail.com>
2024-09-05 04:17:27 +0900

    Final body.py

--- body.py
+++ body.py
@@ -2,29 +2,25 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import math
+from rembg import remove
+from PIL import Image
 
-def find_closest_array(target_array, arrays):
-    """
-    특정 배열의 값들과 비교하여 가장 가까운 값이 많이 존재하는 배열을 반환하는 함수.
+# 이미지 로드
+#input_image = Image.open('/home/mirror/photo.jpg')
+input_image = Image.open('human.jpg')
 
-    :param target_array: 비교 대상이 되는 특정 배열
-    :param arrays: 비교할 3개의 배열이 포함된 리스트
-    :return: 특정 배열의 값들과 가장 가까운 값이 많이 존재하는 배열
-    """
-    closest_counts = [0] * len(arrays)
-    
-    for value in target_array:
-        # 각 value에 대해 모든 배열에서 가장 가까운 값 찾기
-        closest_distances = [min(abs(value - arr_value) for arr_value in array) for array in arrays]
-        closest_index = closest_distances.index(min(closest_distances))
-        closest_counts[closest_index] += 1
-    
-    # 가장 많은 가까운 값이 있는 배열의 인덱스 찾기
-    best_match_index = closest_counts.index(max(closest_counts))
-    return best_match_index
+# 배경 제거
+output_image = remove(input_image)
 
+# 이미지 저장
+output_image.save('human.png')
+
+print(np.__version__)
 #신체 요소 길이 측정
-factor = np.zeros(10) 
+factor = np.zeros(11) 
+
+#체형 분류 0:큰삼각체 1:역삼각체 2:사각체
+category = np.zeros(3)
 
 # Mediapipe 솔루션 초기화
 mp_drawing = mp.solutions.drawing_utils
@@ -32,8 +28,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 # 이미지 파일 경로
-IMAGE_FILE = 'image.jpg'
-#cap = cv2.VideoCapture(0)
+IMAGE_FILE = 'human.png'
 
 # Mediapipe Pose 설정
 with mp_pose.Pose(
@@ -82,19 +77,20 @@ with mp_pose.Pose(
             person_segment = cv2.bitwise_and(image, image, mask=mask)
         
             #좌표 신체 요소로 추출
-            factor[1] = location[29][1] - location[11][1] #어깨높이
+            factor[1] = round(location[29][1] - location[11][1]) #어깨높이
             
-            under_x = abs(location[15][0] - location[13][0])**2
-            under_y = abs(location[13][1] - location[15][1])**2
-            factor[3] = math.sqrt(under_x + under_y) #아래팔길이
+            under_x = round(location[15][0] - location[13][0])**2
+            under_y = round(abs(location[13][1] - location[15][1]))**2
+            factor[3] = round(math.sqrt(under_x + under_y)) #아래팔길이
             
-            shoulder_x = (location[11][0] - location[12][0])**2
-            shoulder_y = abs(location[11][1] - location[12][1])**2
-            factor[5] = math.sqrt(shoulder_x + shoulder_y)#어깨 너비
+            shoulder_x = round(location[11][0] - location[12][0])**2
+            shoulder_y = round(abs(location[11][1] - location[12][1]))**2
+
+            factor[5] = round(math.sqrt(shoulder_x + shoulder_y)) #어깨 너비
             
-            up_x = abs(location[13][0] - location[11][0])**2
-            up_y = abs(location[11][1] - location[13][1])**2
-            factor[6] = math.sqrt(up_x + up_y) #위팔길이
+            up_x = round(location[13][0] - location[11][0])**2
+            up_y = round(abs(location[11][1] - location[13][1]))**2
+            factor[6] = round(math.sqrt(up_x + up_y)) #위팔길이
 
             #총길이
             #사진 흑백으로 전환
@@ -110,7 +106,7 @@ with mp_pose.Pose(
                     if (highest_point is None or point[0][1] < highest_point[1]) and point[0][1] != 0:
                         highest_point = tuple(point[0])
             
-             # Haar Cascade 얼굴 검출기 초기화
+            # Haar Cascade 얼굴 검출기 초기화
             face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
             # 흑백 이미지로 변환 (얼굴 검출기는 흑백 이미지를 요구합니다)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -121,54 +117,137 @@ with mp_pose.Pose(
                 # 턱 좌표 계산 (간단히 얼굴 영역의 가장 아래 점으로 가정)
                 jaw_x = x + w // 2
                 jaw_y = y + h
-                factor[2] = jaw_y - highest_point[1]
+                factor[8] = round(jaw_y - highest_point[1])
+                #몸통 수직길이
+                factor[10] = round(location[22][1] - jaw_y)
                 # 턱 좌표에 빨간색 점 그리기
                 cv2.circle(image, (jaw_x, jaw_y), 5, (0, 0, 255), -1)
 
             #총길이
-            factor[7] = location[29][1] - highest_point[1]
+            factor[7] = round(location[29][1] - highest_point[1])
             
             #비율 
-            height = float(input("키를 입력하세요: "))
+            # height = float(input("키를 입력하세요: "))
+            try:
+                with open('/home/mirror/height.txt', 'r') as f:
+                   height = float(f.read().strip())
+            except FileNotFoundError:
+                raise ValueError("Height file not found. Please enter your height using the MagicMirror module.")
             rat = height / factor[7]
 
             #샅높이
-            leg = location[29][1] - location[22][1]
-            body_len = location[23][1] - location[11][1] + 13 / rat # 몸통수직길이(조정치 필요)
-            print(leg * rat, body_len * rat)
-            factor[4] = leg - body_len
+            factor[9] = round(location[29][1] - location[22][1])
             #머리길이
-            #factor[2] = round(jaw_y - highest_point[1])
-            
-            for i in range(1, 8):
-                factor[i] = round(factor[i] * rat, 1)
-            
-            print("어깨높이: ", factor[1])
-            print("머리 길이: ", factor[2])
-            print("아래팔길이: ", factor[3])
-            print("샅높이 - 몸통수직길이: ", factor[4])
-            print("어깨너비: ", factor[5])
-            print("위팔길이: ", factor[6])
-            print("키: ", factor[7])
-            
+            #factor[8] = round(jaw_y - highest_point[1])
+
+            print("키: ", round(factor[7] * rat, 1))
+            print("어깨높이: ", round(factor[1] * rat, 1))
+            print("아래팔길이: ", round(factor[3] * rat, 1))
+            print("어깨너비: ", round(factor[5] * rat, 1))
+            print("위팔길이: ", round(factor[6] * rat, 1))
+            print("머리 길이: ", round(factor[8] * rat, 1))
+            print("샅높이: ", round(factor[9] * rat, 1))
+            print("몸통수직길이: ", round(factor[10] * rat, 1))
+
+            shouldery = round(factor[1] * rat, 1)
+            head = round(factor[8] * rat, 1)
+            under_arm = round(factor[3] * rat, 1)
+            minus = round(factor[9] * rat, 1) - round(factor[10] * rat, 1)
+            shoulderx = round(factor[5] * rat, 1)
+            up_arm = round(factor[6] * rat, 1)
+
+            # 체형 분류
+            if shouldery < 129.4:
+                category[0] += 1
+            elif 129.4 <= shouldery <= 130.4:
+                category[1] += 1
+            elif 130.4 <  shouldery * rat:
+                category[2] += 1
+
+
+            # 머리크기 분류
+            if 22.3 <=  head:
+                category[0] += 1
+            elif  head <= 22.3:
+                category[1] += 1
+                category[2] += 1
+  
+            # 아래팔길이 분류
+            if 24.8 <=  under_arm:
+                category[0] += 1
+            elif under_arm <= 24.4:
+                category[1] += 1
+            elif 24.5 <=  under_arm <= 24.7:
+                category[2] += 1
+
+            # 샅높이-몸통수직길이 분류
+            if 8.2 <= minus:
+                category[0] += 1
+            elif 7.9 <= minus < 8.2:
+                category[1] += 1
+            elif minus < 7.9:
+                category[2] += 1
+
+            # 어깨너비 분류
+            if  35.8 <= shoulderx < 36.8:
+                category[0] += 1
+            elif 36.8 <= shoulderx:
+                category[1] += 1
+            elif shoulderx < 35.8:
+                category[2] += 1
+
+
+            # 위팔길이 분류
+            if 31.0 <= up_arm:
+                category[0] += 1
+            elif 30.5 <= up_arm <= 31.0:
+                category[1] += 1
+                category[2] += 1
+
+            #결과출력
+            max_index = np.argmax(category)
+            if max_index == 0:
+                print("큰삼각체형")
+                bodytype = "largeTri"
+            elif max_index == 1:
+                print("역삼각체형")
+                bodytype = "inverTri"
+            else:
+                print("사각체형")
+                bodytype = "square"
+
+
+            with open('/home/mirror/information.txt', 'w') as f:
+                f.write(f"키: {round(factor[7] * rat, 1)}\n")
+                f.write(f"어깨높이: {round(factor[1] * rat, 1)}\n")
+                f.write(f"아래팔길이: {round(factor[3] * rat, 1)}\n")
+                f.write(f"어깨너비: {round(factor[5] * rat, 1)}\n")
+                f.write(f"위팔길이: {round(factor[6] * rat, 1)}\n")
+                f.write(f"머리 길이: {round(factor[8] * rat, 1)}\n")
+                f.write(f"샅높이: {round(factor[9] * rat, 1)}\n")
+                f.write(f"몸통수직길이: {round(factor[10] * rat, 1)}\n")
+
+            with open('/home/mirror/result.txt', 'w') as f:
+                f.write(f"{bodytype}\n")
+
+
+
+
+            # print(category[0])
+            # print(category[1])
+            # print(category[2])
+
             # 결과 이미지를 화면에 표시합니다.
-            '''cv2.imshow('Annotated Image', annotated_image)
+            cv2.imshow('Annotated Image', annotated_image)
             cv2.waitKey(0)
-            cv2.destroyAllWindows()'''
+            cv2.destroyAllWindows()
             
-            #체형 별 측정값
-            body_tri = [0, 128.3, 22.4, 24.8, 8.3, 36.5, 31.3, 0]
-            body_intri = [0, 129.5, 22.1, 24.2, 8, 36.9, 30.7, 0]
-            body_rec = [0, 130.9, 22.1, 24.7, 7.8, 35.2, 30.7, 0]     
-            body = [body_tri, body_intri, body_rec]
-            
-            idx = find_closest_array(factor, body)
-            if idx == 0:
-                print("삼각형 체형")       
-            elif idx == 1:
-                print("역삼각형 체형")
-            elif idx == 2:
-                print("사각형 체형")
+            # 결과를 이미지에 그리기
+            if highest_point:
+                cv2.circle(image, highest_point, 5, (0, 255, 0), -1)
+            cv2.imshow("Image with Highest Point", image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
                 
             
             
